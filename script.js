b5c68448babea2d057d5dbba42f30338e3599c28
AbhinavRattan27 <83867719+AbhinavRattan27@users.noreply.github.com>
2022-02-27 13:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function button1js(){
+    window.location.replace("https://abhinavrattan27.github.io/The-Choose-The-hand-game/");
+}
+
+function button2js(){
+    document.getElementById("bye").innerHTML="Bye!"
+    window.location.replace("https://www.google.com/");
+}
